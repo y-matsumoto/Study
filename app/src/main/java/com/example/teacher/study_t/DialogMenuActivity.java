fbfbc6ccb297080f35031a1762c621d975434b38
matsumoto <matsumoto@gmail.com>
2015-12-05 15:30:12 +0900

    added of dialog

--- app/src/main/java/com/example/teacher/study_t/DialogMenuActivity.java
+++ app/src/main/java/com/example/teacher/study_t/DialogMenuActivity.java
@@ -28,59 +28,60 @@ public class DialogMenuActivity extends BaseActivity {
         setContentView(R.layout.activity_dialog_menu);
 
         // 表示ラベル取得
-        label = (TextView)findViewById(R.id.label_dialogtext);
+        label = (TextView) findViewById(R.id.label_dialogtext);
 
         // 通常ダイアログ表示ボタンのクリックリスナー設定
-        Button dialogBtn = (Button)findViewById(R.id.dialogButton);
-        dialogBtn.setTag("dialog");
-        dialogBtn.setOnClickListener(new ButtonClickListener());
+        Button dialogBtn = (Button) findViewById(R.id.dialogButton);
+        dialogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
 
         // テキストダイアログ表示ボタンのクリックリスナー設定
-        Button txtDialogBtn = (Button)findViewById(R.id.textDialogButton);
-        txtDialogBtn.setTag("textDialog");
-        txtDialogBtn.setOnClickListener(new ButtonClickListener());
+        Button txtDialogBtn = (Button) findViewById(R.id.textDialogButton);
+        txtDialogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTextDialog();
+            }
+        });
 
         // 単一選択ダイアログ表示ボタンのクリックリスナー設定
-        Button SingleSelectDialogBtn = (Button)findViewById(R.id.singleSelectDialogButton);
-        SingleSelectDialogBtn.setTag("singleSelectDialog");
-        SingleSelectDialogBtn.setOnClickListener(new ButtonClickListener());
+        Button SingleSelectDialogBtn = (Button) findViewById(R.id.singleSelectDialogButton);
+        SingleSelectDialogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSingleSelectDialog();
+            }
+        });
 
         // 日付選択ダイアログ表示ボタンのクリックリスナー設定
-        Button DatePickerDialogBtn = (Button)findViewById(R.id.datePickerDialogButton);
-        DatePickerDialogBtn.setTag("datePickerDialogBtn");
-        DatePickerDialogBtn.setOnClickListener(new ButtonClickListener());
+        Button DatePickerDialogBtn = (Button) findViewById(R.id.datePickerDialogButton);
+        DatePickerDialogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
 
         // 時間選択ダイアログ表示ボタンのクリックリスナー設定
-        Button TimePickerDialogBtn = (Button)findViewById(R.id.timePickerDialogButton);
-        TimePickerDialogBtn.setTag("timePickerDialogBtn");
-        TimePickerDialogBtn.setOnClickListener(new ButtonClickListener());
-
-        // プログレスバーダイアログ表示ボタンのクリックリスナー設定
-        Button ProgressDialogBtn = (Button)findViewById(R.id.progressDialogButton);
-        ProgressDialogBtn.setTag("progressDialogBtn");
-        ProgressDialogBtn.setOnClickListener(new ButtonClickListener());
-
-    }
-
-    // クリックリスナー定義
-    class ButtonClickListener implements View.OnClickListener {
-        // onClickメソッド(ボタンクリック時イベントハンドラ)
-        public void onClick(View v) {
-            String tag = (String)v.getTag();
-            if (tag.equals("dialog")) {
-                showDialog();
-            } else if (tag.equals("textDialog")) {
-                showTextDialog();
-            } else if (tag.equals("singleSelectDialog")) {
-                showSingleSelectDialog();
-            } else if (tag.equals("datePickerDialogBtn")) {
-                showDatePickerDialog();
-            } else if (tag.equals("timePickerDialogBtn")) {
+        Button TimePickerDialogBtn = (Button) findViewById(R.id.timePickerDialogButton);
+        TimePickerDialogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 showTimePickerDialog();
-            } else if (tag.equals("progressDialogBtn")) {
+            }
+        });
+        // プログレスバーダイアログ表示ボタンのクリックリスナー設定
+        Button ProgressDialogBtn = (Button) findViewById(R.id.progressDialogButton);
+        ProgressDialogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 showProgressDialog();
             }
-        }
+        });
     }
 
     // 通常ダイアログの表示
@@ -88,28 +89,24 @@ public class DialogMenuActivity extends BaseActivity {
         AlertDialog.Builder dialog = new AlertDialog.Builder(DialogMenuActivity.this);
         dialog.setTitle("通常ダイアログ");
         dialog.setMessage("選択してください。");
-        dialog.setPositiveButton("OK",new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog,int whichButton) {
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
                 label.setText("通常ダイアログ：OKが選択されました。");
             }
         });
-        dialog.setNegativeButton("NG", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog,int whichButton) {
-                label.setText("通常ダイアログ：NGが選択されました。");
-            }
-        });
+        dialog.setNegativeButton("NG", null);
         dialog.show();
     }
 
     // テキストダイアログの表示
     public void showTextDialog() {
-        final EditText editText=new EditText(DialogMenuActivity.this);
+        final EditText editText = new EditText(DialogMenuActivity.this);
         AlertDialog.Builder dialog = new AlertDialog.Builder(DialogMenuActivity.this);
         dialog.setTitle("テキストダイアログ");
         dialog.setMessage("テキストを入力してください。");
         dialog.setView(editText);
-        dialog.setPositiveButton("OK",new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog,int whichButton) {
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
                 label.setText("テキストダイアログ：" + editText.getText().toString() + "が入力されました。");
             }
         });
@@ -117,22 +114,23 @@ public class DialogMenuActivity extends BaseActivity {
     }
 
     // 単一選択ダイアログの表示
-    final String[] items = new String[]{"もも","うめ","さくら"};
+    final String[] items = new String[]{"もも", "うめ", "さくら"};
     int which = 0;
+
     public void showSingleSelectDialog() {
         AlertDialog.Builder dialog = new AlertDialog.Builder(DialogMenuActivity.this);
         dialog.setTitle("単一選択ダイアログ");
         dialog.setSingleChoiceItems(items
-                ,0
-                ,new DialogInterface.OnClickListener() {
+                , 0
+                , new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int whichButton) {
                         which = whichButton;
                     }
                 }
         );
 
-        dialog.setPositiveButton("OK",new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog,int whichButton) {
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
                 String selected = items[which];
                 label.setText("単一選択ダイアログ：" + selected + "が入力されました。");
             }
@@ -145,14 +143,14 @@ public class DialogMenuActivity extends BaseActivity {
         Calendar cal = Calendar.getInstance();
 
         DatePickerDialog dialog = new DatePickerDialog(DialogMenuActivity.this
-                ,new DatePickerDialog.OnDateSetListener() {
+                , new DatePickerDialog.OnDateSetListener() {
             public void onDateSet(DatePicker picker, int year, int month, int day) {
-                label.setText("日付選択ダイアログ：" + year + "年" + (month+1) + "月" + day + "日");
+                label.setText("日付選択ダイアログ：" + year + "年" + (month + 1) + "月" + day + "日");
             }
         }
-                ,cal.get(Calendar.YEAR)
-                ,cal.get(Calendar.MONTH)
-                ,cal.get(Calendar.DAY_OF_MONTH)
+                , cal.get(Calendar.YEAR)
+                , cal.get(Calendar.MONTH)
+                , cal.get(Calendar.DAY_OF_MONTH)
         );
         dialog.show();
     }
@@ -160,26 +158,27 @@ public class DialogMenuActivity extends BaseActivity {
     // 時間選択ダイアログの表示
     public void showTimePickerDialog() {
         TimePickerDialog dialog = new TimePickerDialog(DialogMenuActivity.this
-                ,new TimePickerDialog.OnTimeSetListener() {
+                , new TimePickerDialog.OnTimeSetListener() {
             public void onTimeSet(TimePicker picker, int hour, int min) {
                 label.setText("時間選択ダイアログ：" + hour + "時" + min + "分");
             }
         }
-                ,0
-                ,0
-                ,true
+                , 0
+                , 0
+                , true
         );
         dialog.show();
     }
 
     // プログレスバーダイアログの表示
-    ProgressDialog  dialog;
+    ProgressDialog dialog;
+
     public void showProgressDialog() {
-        dialog = new ProgressDialog (DialogMenuActivity.this);
+        dialog = new ProgressDialog(DialogMenuActivity.this);
         dialog.setTitle("プログレスバーダイアログ");
         dialog.setMessage("しばらくお待ちください・・");
-        dialog.setIndeterminate(false);
-        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        dialog.setIndeterminate(true);
+        dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
         //dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         dialog.setMax(100);
         dialog.setCancelable(false);
@@ -189,14 +188,14 @@ public class DialogMenuActivity extends BaseActivity {
         });
 
         dialog.show();
-        new Thread(new Runnable(){
-            public void run(){
-                try{
-                    for(int i=0; i< dialog.getMax(); i++){
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    for (int i = 0; i < dialog.getMax(); i++) {
                         dialog.setProgress(i);
                         Thread.sleep(100);
                     }
-                }catch(Exception e){
+                } catch (Exception e) {
 
                 }
                 dialog.dismiss();
